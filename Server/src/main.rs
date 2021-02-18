ebdaa280c88377bfa25a8a7df7c59969cdf0812f
PierreStephaneVoltaire <psvoltaire96@gmail.com>
2021-02-18 00:02:48 -0500

    updated server

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,11 +1,48 @@
+//! Actix web Diesel integration example
+//!
+//! Diesel does not support tokio, so we have to run it in separate threads using the web::block
+//! function which offloads blocking code (like Diesel's) in order to not block the server's thread.
+
 #[macro_use]
 extern crate diesel;
 
 use actix_web::{get, middleware, post, web, App, Error, HttpResponse, HttpServer};
-
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
 use uuid::Uuid;
 
+mod models;
+mod schema;
 
-fn main() {
-    println!("Hello, world!");
+type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    dotenv::dotenv().ok();
+
+    // use env file on dev
+    let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<SqliteConnection>::new(connspec);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    let bind = "127.0.0.1:8080";
+
+    println!("Starting server at: {}", &bind);
+
+    // Start HTTP server
+    HttpServer::new(move || {
+        App::new()
+            // set up DB pool to be used with web::Data<Pool> extractor
+            .data(pool.clone())
+            .wrap(middleware::Logger::default())
+
+    })
+        .bind(&bind)?
+        .run()
+        .await
 }
